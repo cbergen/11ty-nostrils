320b270519692cf2c8bd33f828469565e7d30db2
Cornelius Bergen <cornelius@matchboxcreative.com>
2021-03-07 22:59:47 -0800

    rename input and output directories

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 module.exports = function (config) {
+	// JavaScript that's compiled by TypeScript
+	config.addPassthroughCopy({ "_static/js": "/js" });
 
-    config.setUseGitIgnore(false);
 
-    config.addPassthroughCopy({ 'static/images': '/images' });
-    config.addPassthroughCopy({ 'static/js': '/js' });
+	config.addPassthroughCopy({ "source/images/*": "/images" });
 
-    if (process.env.NODE_ENV === 'dev') {
-        config.addPassthroughCopy({ 'static/css': '/css' });
-    }
+	if (process.env.NODE_ENV === "dev") {
+		// Use uncompiled CSS in dev
+		config.addPassthroughCopy({ "source/css": "/css" });
+	} else {
+		config.addPassthroughCopy({ "_static/css": "/css" });
+	}
 
-    return {
-        templateFormats: ['md', 'html'],
-        dir: {
-            input: 'views',
-        }
-    }
-}
+	return {
+		templateFormats: ["md", "html"],
+		dir: {
+			input: "source",
+		},
+	};
+};
