02ae33333cd34d640b1e1b6a6d98ee476f25b901
Cornelius Bergen <cornelius@matchboxcreative.com>
2021-03-09 15:53:36 -0800

    clean up eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -4,17 +4,12 @@ module.exports = function (config) {
 	config.addShortcode("img", img);
 
 	config.setUseGitIgnore(false);
-	// config.setWatchThrottleWaitTime(150); // in milliseconds
-
-	// JavaScript that's compiled by SWC/spack
-	// config.addWatchTarget("source/js/*.js");
-	// config.addPassthroughCopy({ "source/js/*.js": "/js" });
 
 	// Images compiled by "img" shortcode
 	config.addPassthroughCopy({ "_static/images/*": "/images" });
 
 	// Images not compiled at all
-	// config.addPassthroughCopy({ "source/images/*": "/images" });
+	config.addPassthroughCopy({ "source/images/*": "/images" });
 
 	if (process.env.NODE_ENV === "dev") {
 		// Use uncompiled CSS in dev
