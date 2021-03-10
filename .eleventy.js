c7db5bba101e3bc1c7a2057c6712d55c45c622b8
Cornelius Bergen <cornelius@matchboxcreative.com>
2021-03-09 16:48:02 -0800

    no need to copy compiled css using eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -5,17 +5,13 @@ module.exports = function (config) {
 
 	config.setUseGitIgnore(false);
 
-	// Images compiled by "img" shortcode
-	config.addPassthroughCopy({ "_static/images/*": "/images" });
-
-	// Images not compiled at all
-	config.addPassthroughCopy({ "source/images/*": "/images" });
+	config.addPassthroughCopy({ "source/images/*": "/images" }); // uncompiled
+	config.addPassthroughCopy({ "_static/images/*": "/images" }); // compiled
 
 	if (process.env.NODE_ENV === "dev") {
-		// Use uncompiled CSS in dev
-		config.addPassthroughCopy({ "source/css": "/css" });
+		config.addPassthroughCopy({ "source/css": "/css" }); // uncompiled
 	} else {
-		config.addPassthroughCopy({ "_static/css": "/css" });
+		// Compiled CSS is output directly to the build directory with PostCSS
 	}
 
 	return {
