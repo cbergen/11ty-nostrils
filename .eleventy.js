4f0a5765fe52744ad7e486e0dae2d332c09aef44
Cornelius Bergen <cornelius@matchboxcreative.com>
2021-08-06 14:29:06 -0700

    passthrough only images from build directory for dev

--- .eleventy.js
+++ .eleventy.js
@@ -5,13 +5,14 @@ module.exports = function (config) {
 
 	config.setUseGitIgnore(false);
 
-    config.addPassthroughCopy({ "source/images/*": "/images" });
+    // config.addPassthroughCopy({ "source/images/*": "/images" });
 
-	if (process.env.NODE_ENV === "dev") {
-		config.addPassthroughCopy({ "source/css": "/css" }); // uncompiled
-	} else {
-		// Compiled CSS is output directly to the build directory with PostCSS
-	}
+    if (process.env.NODE_ENV === "dev") {
+        config.addPassthroughCopy({ "source/css": "/css" }); // uncompiled
+        config.addPassthroughCopy({ "build/images": "/images" });
+    } else {
+        // Compiled CSS is output directly to the build directory with PostCSS
+    }
 
 	return {
 		templateFormats: ["md", "html", "js"],
